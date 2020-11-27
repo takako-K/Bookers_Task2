ea31e5a86b839d8ecc57710e44ce175d65882fc8
takako-K <takako.k828@gmail.com>
2020-11-27 12:10:26 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :books
-  
   get 'top' => 'books#top'
+  post 'books' => 'books#create'
+  get 'books' =>'books#new'
+  get 'books'/:id => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_books'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_books'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
