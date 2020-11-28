973923a8e2048178414a30cb47596fadc7583a8d
takako-K <takako.k828@gmail.com>
2020-11-28 12:32:55 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id), notice: "Book was successfully created."
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+    redirect_to book_path(@book.id), notice: "Book was successfully created."
+  else
+    render action: :index
+  end
   end
 
   def show
